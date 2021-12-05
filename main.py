03be780df1ff4b350ab905cf4815dce241197a57
xProioxis <christionb@hotmail.com>
2021-12-04 19:17:59 -0600

    Added Moving to Pawns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 WINDOW = pygame.display.set_mode((800, 600))
@@ -7,9 +8,13 @@ BLACK = (0, 0, 0)
 RED = (255, 87, 51)
 WHITE = (255, 255, 255)
 GRAY = (105, 105, 105)
+YELLOW = (255, 240, 31)
 
 square_list = []
 piece_list = []
+
+piece_x_offset = 10
+piece_y_offset = 12.5
 
 black_pawn_img = pygame.image.load('Assets/BlackPawn.png')
 red_pawn_img = pygame.image.load('Assets/RedPawn.png')
@@ -48,7 +53,7 @@ def make_pieces():
 
 
 def make_squares():
-    global square_list, RED, WHITE, pawn_img
+    global square_list, RED, WHITE, pawn_img, piece_x_offset, piece_y_offset
     id_letter = "a"
     is_white = True
     # defining squares and their methods
@@ -68,36 +73,34 @@ def make_squares():
             # setting pieces for each square on board
             # piece id is formatted as "(piece color)(piece type)"
             if new_square.id[0] == "7":
-                new_square.piece = Piece("rp", red_pawn_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("rp", red_pawn_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id[0] == "2":
-                new_square.piece = Piece("bp", black_pawn_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("bp", black_pawn_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "1a" or new_square.id == "1h":
-                new_square.piece = Piece("br", black_rook_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("br", black_rook_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "8a" or new_square.id == "8h":
-                new_square.piece = Piece("rr", red_rook_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("rr", red_rook_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "1c" or new_square.id == "1f":
-                new_square.piece = Piece("bb", black_bishop_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("bb", black_bishop_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "8c" or new_square.id == "8f":
-                new_square.piece = Piece("rb", red_bishop_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("rb", red_bishop_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "1b" or new_square.id == "1g":
-                new_square.piece = Piece("bk", black_knight_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("bk", black_knight_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "8b" or new_square.id == "8g":
-                new_square.piece = Piece("rk", red_knight_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("rk", red_knight_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "1e":
-                new_square.piece = Piece("bK", black_king_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("bK", black_king_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "8e":
-                new_square.piece = Piece("rK", red_king_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("rK", red_king_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "1d":
-                new_square.piece = Piece("bq", black_queen_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("bq", black_queen_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
             elif new_square.id == "8d":
-                new_square.piece = Piece("rq", red_queen_img, new_square.x - 10, new_square.y - 12.5)
+                new_square.piece = Piece("rq", red_queen_img, new_square.x - piece_x_offset, new_square.y - piece_y_offset)
 
             square_list.append(new_square)
 
             id_num -= 1
         id_letter = chr(ord(id_letter) + 1)
-
-
 
 
 def blit_board():
@@ -107,7 +110,26 @@ def blit_board():
         if square.piece is not None: # if there is a piece on the square
             WINDOW.blit(square.piece.img, [square.piece.x, square.piece.y])
 
-    pygame.display.update()
+
+
+def square_selected():
+    for square in square_list:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if (square.x < mouse_x < square.x + 75) and (square.y < mouse_y < square.y + 75) and pygame.mouse.get_pressed()[0]:
+            return True, square
+    return False, None
+
+
+def get_path(square):
+    global square_list
+    possible_spaces = []
+    if square.piece.id == "bp":
+        new_ids = [str(int(square.id[0]) + 1) + square.id[1]]
+        for new_square in square_list:
+            if new_square.id in new_ids:
+                possible_spaces.append(new_square)
+
+    return tuple(possible_spaces)
 
 
 make_squares()
@@ -115,16 +137,38 @@ for i in range(len(square_list)):
     print(square_list[i].id, square_list[i].x, square_list[i].y, square_list[i].color)
 
 
-
 running = True
+square_active = False, None
 while running:
     WINDOW.fill(GRAY)
+    blit_board()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-    blit_board()
+
+    if square_active[0]: # if a square is currently being clicked
+        if square_active[1].piece is not None:
+            for square in get_path(square_active[1]): # will return the possible space a piece can move
+                pygame.draw.rect(WINDOW, YELLOW, (square.x, square.y, 75, 75))
+                if square.piece is not None:
+                    WINDOW.blit(square.piece.img, (square.x, square.y, square.x - piece_x_offset, square.y - piece_y_offset))
+
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                if (square.x < mouse_x < square.x + 75) and (square.y < mouse_y < square.y + 75) and pygame.mouse.get_pressed()[0]:
+                    square.piece = Piece(square_active[1].piece.id, square_active[1].piece.img, square.x - piece_x_offset, square.y - piece_y_offset)
+                    square_active[1].piece = None
+                    square_active = False, None
+        if pygame.mouse.get_pressed()[2]:
+            square_active = False, None
+    else:
+        square_active = square_selected() # will determine if square is being clicked
+
+
+
+
+
     pygame.display.update()
 
